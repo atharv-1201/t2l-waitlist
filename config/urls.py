039ecfedf7381960e9116ac3ab4d3e59cad8335b
Atharv Dwivedi <ad5066@srmist.edu.in>
2025-07-08 14:59:22 +0530

    Render files

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from waitlist import views
 from waitlist.views import welcome_message
 
 urlpatterns = [
-    path('', welcome_message, name='welcome'), 
+    path('', welcome_message, name='welcome'),  
     path('admin/', admin.site.urls),
     path('api/', include('waitlist.urls')),
+    path('api/welcome/', views.welcome_message, name='welcome_message'),
 ]
