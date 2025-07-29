d4f70734cb0c0cd731834f954c578c215a98d229
Atharv Dwivedi <ad5066@srmist.edu.in>
2025-07-30 03:39:34 +0530

    Deploy on render

--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- waitlist/serializers.py
+++ waitlist/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from .models import WaitlistEntry
 
 class WaitlistEntrySerializer(serializers.ModelSerializer):
-    interests = serializers.CharField(required=False, allow_blank=True)
+    interests = serializers.CharField(required=False, allow_blank=True, default="")
+
     class Meta:
         model = WaitlistEntry
         fields = ['full_name', 'email', 'location', 'role', 'interests']
